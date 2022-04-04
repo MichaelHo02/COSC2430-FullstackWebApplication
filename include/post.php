4198145c9207628a9297cdec9e849d4befba1615
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-04 16:21:44 +0700

    Add posting section

---
+++ include/post.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <form action="" method="post">
+
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Choose image to upload</label>
+            <input type="file" name="" id="" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="post-desc" class="form-label">Photo description</label>
+            <textarea class="form-control" id="post-desc" rows="3"></textarea>
+        </div>
+
+        <button name="submit" type="submit" class="btn btn-primary post-submit"><i class="bi bi-send me-2"></i>Post</button>
+    </form>
+</div>
