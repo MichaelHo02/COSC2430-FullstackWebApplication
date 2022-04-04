9853b4a32646477fe645f9f491ddc6199048b0d1
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-04 22:12:28 +0700

    Fix navbar link bug

--- include/navbar.php
+++ include/navbar.php
@@ -1,13 +1,28 @@
 <header class="container">
     <nav class="navbar navbar-expand-lg navbar-light position-relative">
         <!-- Bug when clicking at 1nstagram at index.php -->
-        <a href="../index.php" class="navbar-brand display-1 font-weight-bold position-absolute">1nstagram</a> 
+        <a href="<?php 
+            if (str_contains($_SERVER['PHP_SELF'], 'index.php')) {
+                echo "./index.php";
+            } else {
+                echo "../index.php";
+            }
+        ?>" class="navbar-brand display-1 font-weight-bold position-absolute">
+            1nstagram
+        </a> 
         <div class="ms-auto me-auto w-50">
             <form class="">
                 <input class="form-control modal-xl" type="search" placeholder="Search" aria-label="Search">
             </form>
         </div>
-        <a class="btn btn-primary position-absolute end-0" href="./include/login.php">Login</a>
-        <!-- <a class="nav-link" href="./include/login.php">Login</a> -->
+        <a class="btn btn-primary position-absolute end-0" href="
+        <?php 
+            if (str_contains($_SERVER['PHP_SELF'], 'index.php')) {
+                echo "./include/login.php";
+            } else {
+                echo "./login.php";
+            }
+        ?>" >Login</a>
+
     </nav>
 </header>
