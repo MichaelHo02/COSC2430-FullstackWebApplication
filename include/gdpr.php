28fc6769627bbeb2512a35a21bb7144adc12b731
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-04 14:34:08 +0700

    Add gdpr logic

--- include/gdpr.php
+++ include/gdpr.php
@@ -2,6 +2,6 @@
     <div class="card-body">
         <h5 class="card-title">We use cookies</h5>
         <p class="card-text">Our website use cookies to ensure smooth user's experience. By continue browsing our website, you consent to our use of cookies.</p>
-        <a href="#" class="btn btn-warning">I understand</a>
+        <a href="#" class="btn btn-warning gdpr-btn">I understand</a>
     </div>
 </div>
