43e6a33b0c64d5b8d3e4cb431abebac4945be00d
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-04 17:24:27 +0700

    update card ui

--- include/card.php
+++ include/card.php
@@ -1,8 +1,8 @@
-<div class="card col-lg-6 cold-md-6 col-sm-12 mt-3 pt-1 ps-1 pe-1">
+<div class="card col-lg-6 cold-md-6 col-sm-12 pt-0 ps-0 pe-0 rounded-0">
         <img src="./assets/img/stock/noimg.jpeg" class="card-img-top" alt="...">
         <div class="card-body">
             <!-- <h5 class="card-title">Card title</h5> -->
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary rounded-pill">Go somewhere</a>
         </div>
     </div>
