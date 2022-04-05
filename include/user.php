d6a7ee7b00c694456e3df62f74f081c186adf25c
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-05 11:15:20 +0700

    Update user.php

--- include/user.php
+++ include/user.php
@@ -1,74 +1,118 @@
 <?php
-    include "./photo.php";
+include "./photo.php";
 
-    class User {
-        private $id;
-        public $name;
-        private $email;
-        private $username;
-        private $password;
-        private $registeredTime;
-        private $photoIdList;
+class User
+{
+    private $id;
+    public $name;
+    private $email;
+    private $username;
+    private $password;
+    private $registeredTime;
+    private $photoIdList;
+    private $firstName;
+    private $lastName;
+    private $avatar;
 
-        function __construct($name, $email, $username, $password, $registeredTime) {
-            $this->id = "user_" . uniqid();
-            $this->name = $name;
-            $this->email = $email;
-            $this->username = $username;
-            $this->password = $password;
-            $this->registeredTime = $registeredTime;
-            $this->photoIdList = array();
-        } 
+    // function __construct($name, $email, $username, $password, $registeredTime) {
+    //     $this->id = "user_" . uniqid();
+    //     $this->name = $name;
+    //     $this->email = $email;
+    //     $this->username = $username;
+    //     $this->password = $password;
+    //     $this->registeredTime = $registeredTime;
+    //     $this->photoIdList = array();
+    // } 
 
-
-
-        function setName($name) {
-            $this->name = $name;
-        }
-
-        function setEmail($email) {
-            $this->email = $email;
-        }
-
-        function setPassword($password) {
-            $this->password = $password;
-        }
-
-        function setRegistrationTime($registeredTime) {
-            $this->registeredTime = $registeredTime;
-        }
-
-        function addPhoto($photo) {
-            $photoId = $photo->getPhotoId();
-            array_push($this->photoIdList, $photoId);
-        }
-
-        function getId() {
-            return $this->id;
-        }
-        function getName() {
-            return $this->name;
-        }
-
-        function getEmail() {
-            return $this->email;
-        }
-
-        function getUsername() {
-            return $this->username;
-        }
-
-        function getPassword() {
-            return $this->password;
-        }
-
-        function getRegistrationTime() {
-            return $this->registeredTime;
-        }
-
-        public function jsonSerialize() {
-            $vars = get_object_vars($this);
-            return $vars;
-        }        
+    function __construct($email, $password, $firstName, $lastName)
+    {
+        $this->id = "user_" . uniqid();
+        $this->email = $email;
+        $this->password = $password;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
     }
-?>
+
+
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    function setRegistrationTime($registeredTime)
+    {
+        $this->registeredTime = $registeredTime;
+    }
+
+    function addPhoto($photo)
+    {
+        $photoId = $photo->getPhotoId();
+        array_push($this->photoIdList, $photoId);
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getEmail()
+    {
+        return $this->email;
+    }
+
+    function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    function getUsername()
+    {
+        return $this->username;
+    }
+
+    function getPassword()
+    {
+        return $this->password;
+    }
+
+    function getRegistrationTime()
+    {
+        return $this->registeredTime;
+    }
+
+    public function jsonSerialize()
+    {
+        $vars = get_object_vars($this);
+        return $vars;
+    }
+}
