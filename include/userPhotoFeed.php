7059c125025ef5b50c208075b2615e3dde2e38c1
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-06 11:43:25 +0700

    add user photo backend

---
+++ include/userPhotoFeed.php
@@ -0,0 +1,21 @@
+<?php
+    //Fetch all Photo
+    include_once 'sortCmp.php';
+    $photoFile = '../photos.db';
+    $photoDb = fopen($photoFile, 'r');
+    $photoSrc = fread($photoDb, filesize($photoFile));
+    $jsonObj = json_decode($photoSrc);
+    $result = array();
+    usort($jsonObj, "cmpPhoto");
+
+    if (isset($_COOKIE['user-id-cookie'])) { // Logged in -> internal and public
+        for ($i = 0; $i < count($jsonObj); $i++) {
+            if ($jsonObj[$i]->userId == $_COOKIE['user-id-cookie']) {
+                $result[] = $jsonObj[$i];
+            }
+        }
+    } 
+    echo json_encode($result);
+    fclose($photoDb);
+    
+?>
