152e9135410244062525ff69e639fed8f748d356
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-10 13:41:39 +0700

    delete commment prompt

--- include/cardComponent.php
+++ include/cardComponent.php
@@ -115,33 +115,3 @@ function configComponent($postFile, $userFile, $currentPath, $pathToImage)
     </div>';
 }
 ?>
-<!-- 
-<div class="col position-relative">
-    <div class="card h-100">
-        <button type="button" class="btn btn-danger rounded-circle position-absolute top-0 start-100 translate-middle px-2 py-1">
-            <i class="bi bi-x"></i>
-        </button>
-        <div class="card-body">
-            <link rel="stylesheet" href="./assets/css/myaccount.css">
-            <div class="row px-3 align-items-center justify-content-start">
-                <div class="col col-2 bg-info rounded-circle avatar">
-                    <img src="./assets/img/avatar/624edb6ded866.jpg" class="mx-auto d-block img-fluid" alt="avatar">
-                </div>
-                <div class="col col-8">
-                    <h5 class="card-title">Thach Ho</h5>
-                    <p class="card-text">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </p>
-                </div>
-            </div>
-            <div class="row px-3 pt-2">
-                <p class="card-text p-0">
-                    This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.
-                </p>
-            </div>
-        </div>
-        <img src="./assets/img/stock/noimg.jpeg" class="card-img-bottom cap" alt="Camera" />
-    </div>
-</div>
- -->
