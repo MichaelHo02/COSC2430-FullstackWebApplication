5f867170d4595ca856a902b4d39a7c50cf235f0b
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-14 11:15:35 +0700

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../app'));
+const DS = DIRECTORY_SEPARATOR;
+require APPLICATION_PATH . DS . 'config' . DS . 'config.php';
+
+$page = get('page', 'home');
+$model = $config['MODEL_PATH'] . $page . '.php';
+$view = $config['VIEW_PATH'] . $page . '.php';
+$_404 = $config['VIEW_PATH'] . '404.php';
+
+$content = $_404;
+
+if (file_exists($model) && file_exists($view)) {
+    require $model;
+    $content = $view;
+}
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0 ");
+
+include $config['VIEW_PATH'] . 'layout.php';
