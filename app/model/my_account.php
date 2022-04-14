197d6c3288b47447dc5b5966f7bbf2da273e3801
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-14 14:51:34 +0700

    update avt on myacc

---
+++ app/model/my_account.php
@@ -0,0 +1,76 @@
+<!-- Idea: Show avatar, name, email, all posts, logout at the end -->
+<?php
+if (isset($_FILES['formFile'])) {
+    $avatar = $_FILES['formFile'];
+    $file_ok = true;
+    $extension = ['jpg', 'jpeg', 'png', 'gif'];
+    $file_ext = strtolower(end(explode('.', $avatar['name'])));
+    if (in_array($file_ext, $extension) === false) {
+        $file_ok = false;
+        $message = 'Wrong file extension! Only JPG, JPEG, PNG, and GIF';
+    }
+    if ($avatar['size'] > 20000000) {
+        $file_ok = false;
+        $message = 'File size is greater than 20MB';
+    }
+    if ($file_ok) {
+        $fileNewName = uniqid() . '.' . $file_ext;
+        $fileFullName = $config['DATABASE_PATH'] . 'img/avatar/' . $fileNewName;
+        if (move_uploaded_file($avatar['tmp_name'], $fileFullName)) {
+            // echo 'success ' . $fileNewName;
+            $message = 'File is saved!';
+        } else {
+            echo 'fail ' . $fileNewName;
+        }
+    }
+
+    if ($file_ok) {
+        $file = $config['DATABASE_PATH'] . 'users.db';
+
+        $db = fopen($file, 'r');
+        if ($db) {
+            $rawContent = fread($db, filesize($file));
+            $objArray = json_decode($rawContent);
+            for ($i = 0; $i < count($objArray); $i++) {
+                if ($objArray[$i]->id == $_COOKIE['user-id-cookie']) {
+                    $objArray[$i]->avatar = $fileNewName;
+                    break;
+                }
+            }
+            $strSrc = json_encode($objArray);
+            fclose($db);
+
+            $in = fopen($file, 'w');
+            if ($in) {
+                fwrite($in, $strSrc);
+            }
+            fclose($in);
+        }
+    }
+}
+
+$file = $config['DATABASE_PATH'] . 'users.db';
+
+if (isset($_COOKIE['user-id-cookie'])) {
+    $db = fopen($file, 'r');
+    $user;
+
+    if ($db) {
+        $rawContent = fread($db, filesize($file));
+        $objArray = json_decode($rawContent);
+
+        for ($i = 0; $i < count($objArray); $i++) {
+            if ($objArray[$i]->id == $_COOKIE['user-id-cookie']) {
+                $avatar = '../app/database/img/avatar/' . $objArray[$i]->avatar;
+                $email = $objArray[$i]->email;
+                $firstName = $objArray[$i]->firstName;
+                $lastName = $objArray[$i]->lastName;
+                $username = $objArray[$i]->username;
+                $registeredTime = $objArray[$i]->registeredTime;
+                break;
+            }
+        }
+        fclose($db);
+    }
+}
+?>
---
+++ app/view/my_account.php
@@ -0,0 +1,115 @@
+<link rel="stylesheet" href="../app/assets/css/myaccount.css">
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="bg-info rounded-circle w-75 avatar mx-auto">
+                <img src="<?php echo $avatar; ?>" class=" mx-auto d-block img-fluid" alt="avatar">
+            </div>
+        </div>
+        <div class="col-lg-9 col-md-6 col-sm-12 ">
+            <h1 class="display-4 text-lg-start text-md-start text-center">
+                <?php
+                echo "@" . $username;
+                ?>
+            </h1>
+            <div class="row mt-lg-3 mt-md-0 mt-0">
+                <div class="col-lg-6 col-md-12 col-sm-12 text-md-start text-center">
+                    <p for="" class="">Email: <span class=""><?php echo $email; ?></span></p>
+
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 text-md-start text-center">
+                    <p for="" class="">Registered on: <span class="">
+                            <?php
+                            echo $registeredTime;
+                            ?>
+                        </span></p>
+
+                </div>
+            </div>
+            <div class="row mt-lg-3 mt-md-0 mt-0">
+                <div class="col-lg-6 col-md-12 col-sm-12 text-md-start text-center">
+                    <p for="" class="">First name: <span class=""><?php echo $firstName; ?></span> </p>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 text-md-start text-center">
+                    <p for="" class="">Last name: <span class=""><?php echo $lastName; ?></span></p>
+
+                </div>
+            </div>
+            <div class="row mt-lg-3 mt-md-0 mt-0">
+                <div class="d-grid gap-2">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary <?php
+                                                                    if (isset($_FILES['formFile'])) {
+                                                                        if ($file_ok) {
+                                                                            echo 'is-valid btn-success';
+                                                                        } else {
+                                                                            echo 'is-invalid btn-danger';
+                                                                        }
+                                                                    }
+                                                                    ?>" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                        Change Avatar
+                    </button>
+                    <div id="" class="<?php
+                                        if (isset($_FILES['formFile'])) {
+                                            if ($file_ok) {
+                                                echo 'valid-feedback';
+                                            } else {
+                                                echo 'invalid-feedback';
+                                            }
+                                        }
+                                        ?>">
+                        <?php
+                        echo $message;
+                        ?>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Change Avatar</h5>
+                                    <button type="button" class="btn-close" aria-label="Close" data-bs-dismiss="modal"></button>
+                                    </button>
+                                </div>
+                                <form action="myaccount.php" name="form" method="post" enctype="multipart/form-data">
+                                    <div class="modal-body">
+                                        <div class="mb-3">
+                                            <label for="formFile" class="form-label">Profile picture</label>
+                                            <input class="form-control <?php
+                                                                        if (isset($_FILES['formFile'])) {
+                                                                            if ($file_ok) {
+                                                                                echo 'is-valid';
+                                                                            } else {
+                                                                                echo 'is-invalid';
+                                                                            }
+                                                                        }
+                                                                        ?>" type="file" id="formFile" name="formFile">
+                                            <div id="" class="<?php
+                                                                if (isset($_FILES['formFile'])) {
+                                                                    if ($file_ok) {
+                                                                        echo 'valid-feedback';
+                                                                    } else {
+                                                                        echo 'invalid-feedback';
+                                                                    }
+                                                                }
+                                                                ?>">
+                                                <?php
+                                                echo $message;
+                                                ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary" value="Submit">Save changes</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
