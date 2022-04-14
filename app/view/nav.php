8c7c3d2c824ba172ec349a52732b45300c2cc0c9
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-14 11:15:29 +0700

    Create nav.php

---
+++ app/view/nav.php
@@ -0,0 +1,15 @@
+<header class="container">
+    <nav class="navbar navbar-expand-lg navbar-light position-relative">
+        <a href="<?php echo $navBrandLink; ?>" class="navbar-brand display-1 font-weight-bold position-absolute">
+            1nstaKilogram
+        </a>
+        <div class="ms-auto me-auto w-50">
+            <form class="">
+                <input class="form-control modal-xl" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+        <a class="btn <?php echo $btnClass; ?> position-absolute end-0" href=" <?php echo $btnLink; ?>">
+            <?php echo $btnInnerText; ?>
+        </a>
+    </nav>
+</header>
