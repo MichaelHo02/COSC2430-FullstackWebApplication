36eaf057f33be5f92e55ef10a72174ebe3072c4a
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-02 18:34:32 +0700

    Fix gdpr bug

--- app/view/layout.php
+++ app/view/layout.php
@@ -44,7 +44,11 @@
         require $config['VIEW_PATH'] . 'footer.php';
         ?>
     </div>
-    <?php require $config['VIEW_PATH'] . 'gdpr.php'; ?>
+    <?php 
+        if (!isset($_COOKIE['gdpr'])) {
+            require $config['VIEW_PATH'] . 'gdpr.php'; 
+        }
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
--- public/assets/js/gdpr.js
+++ public/assets/js/gdpr.js
@@ -2,16 +2,15 @@ const GDPR = document.querySelector(".gdpr-wrapper");
 const GDPRBtn = document.querySelector(".gdpr-btn");
 
 if (localStorage.getItem('gdpr') == undefined) {
-    localStorage.setItem('gdpr', 'true');
+    localStorage.setItem('gdpr', 'false');
+    GDPR.style.display = 'block';
 }
 
-// localStorage.setItem('gdpr', 'true')
 if (localStorage.getItem('gdpr') == 'false') {
-    GDPR.style.display = 'none';
+    GDPR.style.display = 'block';
 }
 
 GDPRBtn.addEventListener('click', () => {
-    localStorage.setItem('gdpr', 'false');
+    localStorage.setItem('gdpr', 'true');
     GDPR.style.display = 'none';
-
 });
