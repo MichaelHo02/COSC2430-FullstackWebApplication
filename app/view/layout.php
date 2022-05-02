d3a04c524d75545bfcf2e19682a8da751a4a5b8a
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-05-02 19:28:53 +0700

    fix gdpr appear in admin

--- app/view/layout.php
+++ app/view/layout.php
@@ -10,8 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/avatar.css">
     <link rel="stylesheet" href="/assets/css/dashboard.css">
-    <link rel="stylesheet" href="/assets/css/gdpr.css">
-    <script src="assets/js/gdpr.js" defer></script>
     <script src="assets/js/deleteCard.js" defer></script>
 </head>
 
@@ -41,11 +39,12 @@
         </main>
 
     </div>
-    <?php 
-        if (!str_contains($page, 'admin')) {
-            require $config['VIEW_PATH'] . 'gdpr.php'; 
-        }
-        
+    <?php
+    if (!str_contains($page, 'admin')) {
+        require $config['VIEW_PATH'] . 'gdpr.php';
+        echo '<link rel="stylesheet" href="/assets/css/gdpr.css">';
+        echo '<script src="assets/js/gdpr.js" defer></script>';
+    }
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
