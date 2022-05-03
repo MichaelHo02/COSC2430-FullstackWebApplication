18a5482be64b3a3b9606d0cf74ed6a157125461f
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-03 14:19:56 +0700

    Clean up code

--- app/view/layout.php
+++ app/view/layout.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/avatar.css">
     <link rel="stylesheet" href="/assets/css/dashboard.css">
+    <script src="assets/js/config.js" defer></script>
     <script src="assets/js/deleteCard.js" defer></script>
 </head>
 
@@ -50,11 +51,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
-
-
-<script>
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
-</script>
 
 </html>
